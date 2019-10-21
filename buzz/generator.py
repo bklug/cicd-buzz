6f9da88882f3b2a9b91cb91ddebb0d6e4d937cbf
bklug <bklug@bcit.ca>
2019-10-21 20:59:42 +0000

    Step 10

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('Bill Klug', 'continuous testing', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
-adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adjectives = ('A00123456','complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
